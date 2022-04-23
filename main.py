597231facce67203cc47359820cde98b585e3839
Sofi B <76660573+Sofia-Zammitto-Benasayag@users.noreply.github.com>
2022-04-23 03:00:37 +0000

    Added drone shipping

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 #Shipping Calculator 
 
 #Variables 
-weight = 20
+weight = float(input("Insert weight: "))
+cost_ground_premium = 125.00
 
 #Ground shipping
 if weight <= 2:
@@ -13,4 +14,17 @@ elif weight <= 10:
 elif weight > 10:
   cost_ground = weight * 4.75 + 20   
 
-print("You're total is $" + str(cost_ground))
+print("Your total for ground shipping is $" + str(cost_ground))
+print("Your total for ground premium is $ " + str(cost_ground_premium))
+
+#Drone shipping
+if weight <= 2:
+  cost_drone = weight * 4.5 + 0
+elif weight <= 6:
+  cost_drone = weight * 9 + 0
+elif weight <= 10:
+  cost_drone = weight * 12 + 0
+elif weight > 10:
+  cost_drone = weight * 14.25 + 0    
+
+print("Your total for drone shipping is $" + str(cost_drone))
